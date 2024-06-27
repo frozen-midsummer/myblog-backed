08a7294cb1ac2ecb1b696c0bd2a4e2dd8300f15c
wjx <610798187@qq.com>
2024-06-27 18:12:35 +0800

    first commit

---
+++ src/main/java/com/wjx/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.wjx.mapper;
+
+import com.wjx.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    List<User> findAllUsers();
+}
---
+++ src/test/java/com/wjx/SpringbootMyblogApplicationTests.java
@@ -0,0 +1,35 @@
+package com.wjx;
+
+import com.wjx.entity.User;
+import com.wjx.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(value = SpringRunner.class)
+@SpringBootTest(classes = SpringbootMyblogApplication.class)
+class SpringbootMyblogApplicationTests {
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void findAllUsers() {
+        System.out.println(userMapper.findAllUsers());
+    }
+    @Test
+    void build(){
+        User user = new User();
+        user.setUsername("wjx");
+        user.setPassword("12546");
+        System.out.println(user.toString());
+    }
+    @Test
+    void passwordEncoded(){
+        System.out.println(passwordEncoder.encode("Wangjiaxuan12"));
+    }
+}
